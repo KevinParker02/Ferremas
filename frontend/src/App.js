497e690024c504aba77608bbc43f4fb2f3db68f8
Sebastian <sebastiancarrera2021@gmail.com>
2025-05-01 22:55:18 -0400

    fix del css del frontend(inutil) Denada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'; 
 import FerremasPage from './components/FerremasPage';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <FerremasPage />
+    <Helmet>
+    </Helmet>
+    <FerremasPage />
     </div>
   );
 }
--- frontend/src/components/FerremasPage.js
+++ frontend/src/components/FerremasPage.js
@@ -59,9 +59,6 @@ const FerremasPage = () => {
       <footer>
         <div className="box-bottom"></div>
       </footer>
-
-      {/* Incluir Bootstrap */}
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     </div>
   );
 };
