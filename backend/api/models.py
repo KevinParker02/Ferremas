12921956eb3ce6906b5c18dd059d1fb126d77268
Sebastian <sebastiancarrera2021@gmail.com>
2025-05-01 23:29:28 -0400

    Añadido rol a la sección de admin

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,3 +6,13 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+class Role(models.Model):
+    id_rol = models.IntegerField(primary_key=True)  # Lo que tenías en MySQL
+    nom_rol = models.CharField(max_length=40)  # Lo que tenías en MySQL
+
+    class Meta:
+        db_table = 'ROL_USER'
+    
+    def __str__(self):
+        return self.nom_rol
