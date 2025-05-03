c6ca2f7ce025b38393a4e63b1c161ca524b9a38b
Sebastian <sebastiancarrera2021@gmail.com>
2025-05-02 22:05:38 -0400

    Rol y comuna a menu admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
-from .models import Role
+from .models import *
+#Aqui registramos los roles
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('id_rol', 'nom_rol')
+    search_fields = ('nom_rol',)
 
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('id_region', 'nom_region')
+    search_fields = ('nom_region',)
 
-admin.site.register(Role)
+@admin.register(Comuna)
+class ComunaAdmin(admin.ModelAdmin):
+    list_display = ('id_comuna', 'nom_comuna', 'region')
+    search_fields = ('nom_comuna',)
+    list_filter = ('region',)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+#Modelo de prueba
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
         return self.name
-
+#modelo de rol
 class Role(models.Model):
     id_rol = models.IntegerField(primary_key=True)  # Lo que tenías en MySQL
     nom_rol = models.CharField(max_length=40)  # Lo que tenías en MySQL
@@ -16,3 +16,24 @@ class Role(models.Model):
     
     def __str__(self):
         return self.nom_rol
+#modelo de region
+class Region(models.Model):
+    id_region = models.IntegerField(primary_key=True)
+    nom_region = models.CharField(max_length=60)
+
+    class Meta:
+        db_table = 'REGION'
+
+    def __str__(self):
+        return self.nom_region        
+#modelo de comuna
+class Comuna(models.Model):
+    id_comuna = models.IntegerField(primary_key=True)
+    nom_comuna = models.CharField(max_length=60)
+    region = models.ForeignKey('Region', on_delete=models.CASCADE, db_column='id_region')
+
+    class Meta:
+        db_table = 'COMUNA'
+
+    def __str__(self):
+        return self.nom_comuna
