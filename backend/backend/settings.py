40d3ecc7198403de23ef71cf28c9147dee91e10b
yas <yas.villarroel@duocuc.cl>
2025-05-08 12:11:36 -0400

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # Motor MySQL
         'NAME': 'ferremas',  # Aquí va el nombre de la base de datos
         'USER': 'root',  # Usuario de MySQL
-        'PASSWORD': '1111',  # Contraseña del usuario
+        'PASSWORD': 'admin',  # Contraseña del usuario
         'HOST': 'localhost',  # Cambia si usas un servidor remoto
         'PORT': '3306',  # El puerto por defecto de MySQL
     }
